66eb9a6a565ab292fba265c6aac90cb9fe0d69b0
Jordan E Medlock <jordanemedlock@gmail.com>
2022-06-07 18:54:13 -0700

    Drew time to alarm in widget

--- apps/tasks/widget.js
+++ apps/tasks/widget.js
@@ -3,13 +3,14 @@
     var titles = [ "Work", "Clean", "Relax", "Game", "Elden Ring", "Watch", "Tik Tok" ];
 
     function draw() {
-        const alarms = require("sched").getAlarms();
+        const alarms = require('Storage').readJSON('sched.json',1) || [];
         const tasks = alarms.filter(a => a.task && a.on);
 
         const task = tasks[0];
         if (task) {
-            g.reset();
-            var text = titles[task.title] + " " + require("time_utils").formatDuration(task.timer);
+            g.reset();var time = new Date();
+            var currentTime = (time.getHours()*3600000)+(time.getMinutes()*60000)+(time.getSeconds()*1000);
+            var text = titles[task.title] + " " + require("time_utils").formatDuration(task.t - currentTime);
             g.setFont("6x8:1x2");
             g.setFontAlign(-1,-1);
             g.drawString(text, this.x, this.y, true);
@@ -20,13 +21,18 @@
         }
     }
 
+    function reload() {
+        this.draw(this);
+    }
+
     setInterval(function() {
-        WIDGETS["alarm"].draw(WIDGETS["alarm"]);
+        WIDGETS["alarm"].reload();
     }, 30*1000);
 
     WIDGETS["alarm"] = {
         area: "tl",
         width: 0,
-        draw: draw
+        draw: draw,
+        reload: reload
     };
 })();
